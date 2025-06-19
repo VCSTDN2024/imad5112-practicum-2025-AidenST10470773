3498a599fae1d6cb64accc808d4d0c3f5a1886c3
Aiden Munsamy <ST10470773@vcconnect.edu.za>
2025-06-19 11:04:27 +0200

    added: review screen

--- app/src/main/java/vcmsa/ci/songmanager/Review_playlist.kt
+++ app/src/main/java/vcmsa/ci/songmanager/Review_playlist.kt
@@ -11,10 +11,6 @@ class Review_playlist : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_review_playlist)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
